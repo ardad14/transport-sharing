d0091f441d5662c6551ff698d6659bc923231905
Artem Kuptsov <artem.kuptsov@nure.ua>
2021-03-31 13:07:14 +0300

    Update environment

---
+++ mobile/src/BicyclePage/BicyclePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text, View, StyleSheet, TextInput, Image, TouchableOpacity} from 'react-native'
+import * as Font from 'expo-font'
+import {Header} from '../Header.js'
+import MapView from 'react-native-maps';
+
+export const BicyclePage = (props) => {
+
+    return (
+        <View>
+            <Header />
+        </View>
+    )
+
+}
